c9c4b1166354810114b9cc2ac2f7c01d7e90dcb6
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2023-11-06 22:58:08 -0300

    feature - suma de matrices

--- controllers/matrix.controllers.js
+++ controllers/matrix.controllers.js
@@ -1,17 +1,35 @@
-import { Matrix } from "../model/matrix.class.js";
+import { Matrix } from "../models/models.matrix.js";
 
 let selectedMx = false;
 
 //#region devTools
 
-const loadMxs = () => {
+const autoLoadMxs = () => {
     const mxs = [
         [3, 4, 'MxA'],
-        [5, 3, 'MxB'],
+        [5, 3, 'MxB', [
+            [5, 4, 6],
+            [9, 6, 8],
+            [1, 0, 4],
+            [8, 1, 6],
+            [7, 6, 0]
+        ]],
+        [2, 2, 'MxC'],
         [2, 2, 'MxD'],
-        [3, 4, 'MxE'],
+        [4, 4, 'MxE'],
         [7, 3, 'MxF'],
-        [4, 3, 'MxG']
+        [3, 3, 'MxG'],
+        [5, 5, 'MxH'],
+        [7, 7, 'MxI'],
+        [10, 10, 'MxJ'],
+        [5, 3, 'MxL', [
+            [2, 3, 5],
+            [5, 7, 6],
+            [1, 2, 6],
+            [3, 8, 4],
+            [1, 8, 0]
+        ]],
+        [3, 7, 'MxK']
     ];
 
     mxs.forEach(mx => {
@@ -20,11 +38,44 @@ const loadMxs = () => {
         inpName.value = mx[2];
         btnSave.click();
     });
+
+    const mxsToFill = mxs.filter(mx => mx[3]);
+    mxsToFill.forEach((mx) => {
+        const divSelected = divListMatrices.children[mxs.indexOf(mx)];
+        divSelected.click();
+        const cells = [...document.querySelectorAll('.celda')];
+        console.log(cells);
+        console.log(cells[5]);
+        for (let i = 0; i < mx[0]; i++) {
+            for (let j = 0; j < mx[1]; j++) {
+                console.log(`(${i}x${j})`);
+                console.log('(i * mx[1]) + j = ', (i * mx[1]) + j);
+                console.log('cells[(i * mx[1]) + j] = ', cells[(i * mx[1]) + j]);
+                cells[(i * mx[1]) + j].value = mx[3][i][j];
+            }
+        }
+        selectedMx = divSelected;
+        btnSaveValues.disabled = false;
+        btnSaveValues.click();
+    });
 };
+
+const autoOperarMxs = (indexs, operation) => {
+    const eventChange = new Event('change');
+    selectListOperations.selectedIndex = 1;
+    selectListOperations.value = operation;
+    selectListOperations.dispatchEvent(eventChange);
+    indexs.forEach(ind => {
+        selectListMxsOperations.value = ind;
+        selectListMxsOperations.dispatchEvent(eventChange);
+    });
+    btnOpeartions.click();
+}
 
 // autoCompletar valores para hagilizar desarrollo
 document.addEventListener('DOMContentLoaded', () => {
-    loadMxs();
+    // autoLoadMxs();
+    // autoOperarMxs([1, 10], 'sum');
 })
 
 //#endregion
@@ -44,58 +95,52 @@ const lblDisMx = document.querySelector('#lblDisplayMx');
 const btnSaveValues = document.querySelector('#saveValues');
 const mxDisMx = {
     elm: document.querySelector('#vlvDisplayMx'),
-    fill({ mx, rows, columns }){
-        let sizeHTML = this.elm.children.length;
-        const sizeNew = mx.length * mx[0].length;
-
-        // agregar o sacar celdas
-        if (sizeHTML > sizeNew) {
-            while(this.elm.children.length != sizeNew){
-                this.elm.removeChild(this.elm.lastChild);
-            }
-        } else if (sizeHTML < sizeNew) {
-            while (sizeHTML != sizeNew) {
-                const inp = document.createElement('input');
-                inp.classList.add('celda');
-                inp.addEventListener('change', changeCells)
-                inp.type = 'number';
-                this.elm.appendChild(inp);
-                sizeHTML++;
-            }
-        }
-
-        // setear valores de celdas
-        const inputs = this.elm.children;
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
-                const value = mx[i][j];
-                inputs[(i * columns) + j].value = value;
-            }
-        }
-    },
-    visible(mx){
-        if(mx){
-            this.elm.style.gridTemplateRows = `repeat(${mx.rows}, 25px)`;
-            this.elm.style.gridTemplateColumns = `repeat(${mx.columns}, 25px)`;
+    visible(matrix){
+        if(matrix){
+            console.log('desde visible', matrix);
+            lblDisMx.innerHTML = matrix.name;
+            const { mx, rows, columns } = matrix;
+            this.elm.style.gridTemplateRows = `repeat(${rows}, 25px)`;
+            this.elm.style.gridTemplateColumns = `repeat(${columns}, 25px)`;
             this.elm.classList.remove('hidden');
-            this.fill(mx);
+            let cntInps = this.elm.children.length;
+            const cntCells = mx.length * mx[0].length;
+
+            // agregar o sacar celdas
+            if (cntInps > cntCells)
+                while(this.elm.children.length != cntCells)
+                    this.elm.removeChild(this.elm.lastChild);
+            else if (cntInps < cntCells)
+                while (cntInps != cntCells) {
+                    const inp = document.createElement('input');
+                    inp.classList.add('celda');
+                    inp.addEventListener('change', changeCells)
+                    inp.type = 'number';
+                    this.elm.appendChild(inp);
+                    cntInps++;
+                }
+
+            // setear valores de celdas
+            const inputs = this.elm.children;
+            for (let i = 0; i < rows; i++) {
+                for (let j = 0; j < columns; j++) {
+                    const value = mx[i][j];
+                    inputs[(i * columns) + j].value = value;
+                }
+            }
         } else this.elm.classList.add('hidden');
     }
 }
 
-// Suma
-const selectListMxSuma = document.querySelector('#listMxsOperatonSuma');
-const ulListMxSelectedSuma = document.querySelector('#listMxSelectedSuma');
+// Operaciones
+const selectListOperations = document.querySelector('#listOperations');
+const selectListMxsOperations = document.querySelector('#listMxsOperations');
+const ulListMxSelectedOperations = document.querySelector('#listMxSelectedOperations');
+const btnOpeartions = document.querySelector('#btnOperations');
+const btnOpeartionsUnary = document.querySelector('#btnOperationsUnary');
 
-// Resta
-const selectListMxResta = document.querySelector('#listMxsOperatonResta');
-const ulListMxSelectedResta = document.querySelector('#listMxSelectedResta');
-
-// Determinante
-const mxSelectedDet = document.querySelector('#inpMxSelected-determinant');
-
-// Cuadrado Magico
-const mxSelectedSqr = document.querySelector('#inpMxSelected-magicSquare');
+// Operaciones Unarias
+const selectListOperationsUnary = document.querySelector('#listOperations');
 
 //#endregion
 
@@ -105,35 +150,27 @@ const removeMxListOperation = e => {
     const index = e.target.id.replace('quitMxSelected', '');
     const li = e.target.parentNode;
     const ul = li.parentNode;
-    const operation = e.target.parentNode.id.includes('Suma'); // true: suma ; false: resta
     const name = li.childNodes[0].textContent;
 
-    if(ul.firstChild === li && ul.children.length > 1)
-        ul.removeChild(li.nextSibling);
-    if(ul.children.length > 1 && ul.firstChild !== li){
-        ul.removeChild(li.previousSibling);
-    }
+    if(ul.firstChild === li && ul.children.length > 1) ul.removeChild(li.nextSibling);
+    if(ul.children.length > 1 && ul.firstChild !== li) ul.removeChild(li.previousSibling);
     ul.removeChild(li);
-    if(ul.children.length === 0){
-        ul.classList.add('hidden');
-    }
+    if(ul.children.length === 0) ul.classList.add('hidden');
 
     const option = document.createElement('option');
         option.value = index;
         option.innerText = name;
 
-    if(operation) selectListMxSuma.appendChild(option);
-    else selectListMxResta.appendChild(option);
+    selectListMxsOperations.appendChild(option);
 }
 
 const selectItemOperation = e => {
     const select = e.target;
     const indexMx = select.value;
     const itemSelected = select.options[select.selectedIndex];
-    const operation = e.target.id.includes('Suma') ? 'Suma' : 'Resta';
 
     select.removeChild(itemSelected);
-    const listMx = select.parentNode.children[1];
+    const listMx = ulListMxSelectedOperations;
     listMx.classList.remove('hidden');
     const button = document.createElement('button');
         button.classList.add('listMxs__button-quit');
@@ -142,7 +179,7 @@ const selectItemOperation = e => {
         button.addEventListener('click', removeMxListOperation);
     const li = document.createElement('li');
         li.classList.add('listMxsOperation__item');
-        li.id = `mxOperation${operation}${indexMx}`;
+        li.id = `mxOperation${indexMx}`;
         li.innerText = itemSelected.innerText;
         li.appendChild(button);
 
@@ -153,7 +190,6 @@ const selectItemOperation = e => {
     }
 
     listMx.appendChild(li);
-
 }
 
 const changeCells = () => {
@@ -175,9 +211,11 @@ const selectMxInList = e => {
 
     selectedMx = divSelected;
     selectedMx.classList.add('listMxs__item-selected');
-    lblDisMx.innerText = mxSelectedDet.value = mxSelectedSqr.value = selectedMx.childNodes[1].innerText;
+    const textNodeOfName = selectedMx.childNodes[1];
+    lblDisMx.innerText = textNodeOfName.innerText;
 
-    const mx = Matrix.list[selectedMx.id];
+    const index = selectedMx.id.replace('mxList', '');
+    const mx = Matrix.list[index];
     mxDisMx.visible(mx);
 
 }
@@ -190,7 +228,7 @@ const isValidated = (rows, columns, name) => {
     if(name.length > 10) return 'Nombre muy largo.';
 
     for (let i = 0; i < Matrix.list.length; i++)
-        if(name === Matrix.list[i].name)
+        if(name.toLowerCase() === Matrix.list[i].name.toLowerCase())
             return 'Nombre repetido.';
 
     return false;
@@ -199,32 +237,25 @@ const isValidated = (rows, columns, name) => {
 const loadList = () => {
 
     // borra todas las matrices de las listas
-    while (divListMatrices.firstChild || selectListMxResta.firstChild || selectListMxSuma.firstChild || ulListMxSelectedSuma.firstChild || ulListMxSelectedResta.firstChild){
-        if(ulListMxSelectedSuma.firstChild) ulListMxSelectedSuma.removeChild(ulListMxSelectedSuma.firstChild);
-        if(ulListMxSelectedResta.firstChild) ulListMxSelectedResta.removeChild(ulListMxSelectedResta.firstChild);
+    while (divListMatrices.firstChild || selectListMxsOperations.firstChild || ulListMxSelectedOperations.firstChild){
         if(divListMatrices.firstChild) divListMatrices.removeChild(divListMatrices.firstChild);
-        if(selectListMxResta.firstChild) selectListMxResta.removeChild(selectListMxResta.firstChild);
-        if(selectListMxSuma.firstChild) selectListMxSuma.removeChild(selectListMxSuma.firstChild);
+        if(selectListMxsOperations.firstChild) selectListMxsOperations.removeChild(selectListMxsOperations.firstChild);
+        if(ulListMxSelectedOperations.firstChild) ulListMxSelectedOperations.removeChild(ulListMxSelectedOperations.firstChild);
     }
 
-    ulListMxSelectedSuma.classList.add('hidden');
-    ulListMxSelectedResta.classList.add('hidden');
+    ulListMxSelectedOperations.classList.add('hidden');
 
     let option = document.createElement('option');
         option.value = -1;
         option.innerText = 'Seleccione Matrices';
-    selectListMxSuma.appendChild(option);
-    option = document.createElement('option');
-        option.value = -1;
-        option.innerText = 'Seleccione Matrices';
-    selectListMxResta.appendChild(option);
+    selectListMxsOperations.appendChild(option);
 
     // carga todas las matrices desde lista nueva o modificada
     Matrix.list.forEach( (mx, index) => {
         // lista de seleccion
         const divMx = document.createElement('div');
             divMx.classList.add('listMxs__item');
-            divMx.id = index;
+            divMx.id = `mxList${index}`;
             divMx.addEventListener('click', selectMxInList, false);
 
         const p = document.createElement('p');
@@ -244,15 +275,10 @@ const loadList = () => {
         option = document.createElement('option');
             option.value = index;
             option.innerText = mx.name;
-        selectListMxResta.appendChild(option);
-        option = document.createElement('option');
-            option.value = index;
-            option.innerText = mx.name;
-        selectListMxSuma.appendChild(option);
+            selectListMxsOperations.appendChild(option);
     });
 
     selectedMx = false;
-    mxSelectedDet.value = mxSelectedSqr.value = "";
     lblDisMx.innerText = "Seleccione una matriz";
     mxDisMx.visible(false);
 }
@@ -264,7 +290,9 @@ const loadList = () => {
 // Guardar celdas
 btnSaveValues.addEventListener('click', () => {
     if(!selectedMx) return;
-    const { mx, rows, columns} = Matrix.list[selectedMx.id];
+    const index = selectedMx.id.replace('mxList', '');
+    console.log(Matrix.list[index]);
+    const { mx, rows, columns} = Matrix.list[index];
     const cellsValue = [...mxDisMx.elm.children]
         .map(inp => inp.value.trim() !== '' ? parseFloat(inp.value) : null);
 
@@ -290,8 +318,8 @@ const deleteMxOnList = e => {
 
 // Agregar
 btnSave.addEventListener('click', () => {
-    const rows = inpRows.value;
-    const columns = inpColumns.value;
+    const rows = parseInt(inpRows.value);
+    const columns = parseInt(inpColumns.value);
     const name = inpName.value;
 
     inpName.focus();
@@ -309,7 +337,26 @@ btnSave.addEventListener('click', () => {
     inpRows.value = inpColumns.value = inpName.value = '';
 })
 
-selectListMxSuma.addEventListener('change', selectItemOperation);
-selectListMxResta.addEventListener('change', selectItemOperation);
+selectListMxsOperations.addEventListener('change', selectItemOperation);
+selectListOperations.addEventListener('change', () => {
+    const { value } = selectListOperations.options[selectListOperations.selectedIndex];
+
+    if(value < 0) btnOpeartions.classList.add('hidden')
+    else btnOpeartions.classList.remove('hidden')
+});
+
+btnOpeartions.addEventListener('click', () => {
+    const mxs = [...ulListMxSelectedOperations.children]
+        .filter(li => li.tagName === 'LI')
+        .map(li => {
+            const index = li.id.replace('mxOperation', '');
+            return Matrix.list[index];
+    });
+
+    console.log(mxs);
+    const MXrESULTANTE = Matrix.sumAll(...mxs);
+    console.log('mxResult = ', MXrESULTANTE);
+    mxDisMx.visible(MXrESULTANTE);
+})
 
 //#endregion
